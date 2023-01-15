3e640f32ba229a8ed13ee2def2e69caf4a65a8ed
Ken Manheimer <ken.manheimer@gmail.com>
2023-01-15 15:49:06 +0000

    Remove isInternalURL import.
    I haven't found a way to use it to help maintain the presence of the red
    underline indicating that the entry is in the visited-page path.

--- src/customizations/components/theme/Navigation/NavItems.jsx
+++ src/customizations/components/theme/Navigation/NavItems.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import NavItem from '@plone/volto/components/theme/Navigation/NavItem';
-import { isInternalURL } from '@plone/volto/helpers';
 import { Dropdown } from 'semantic-ui-react';
 import './dropdownmenu.css';
 
