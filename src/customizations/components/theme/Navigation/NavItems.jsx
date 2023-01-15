99cc5a050da41ebf4a68a66a06e2991c96f02a5d
Ken Manheimer <ken.manheimer@gmail.com>
2023-01-15 13:56:35 +0000

    Implement select-on-containing-entry, but stubborn.
    Incorporate NavLink behavior in the Dropdown base entry using the `as=`
    attribute. We can now click on the base entry in order to transit to that
    page. Yay!
    
    An unintended consequence is that the menus are automatically and
    persistently open to present the currently visited page.
    
    The problem may be connected with the `item.active` class being
    automatically acquired by NavLinks that qualify for
    `isInternalURL()` (found in @plone/volto/helpers), while `active` is also
    used by `simple` Dropdownmenus for menus that are open.
    
    It may instead be that the `as={NavLink}` disrupts the `simple`
    Dropdownmenu closing behavior, but that wouldn't explain why the
    menus (sometimes) open to the entry for the current page when navigating to
    it using the URL directly, or the back or forward buttons, etc.
    
    If nothing else, this implementation demonstrates an approach that mostly
    works.

--- src/customizations/components/theme/Navigation/NavItems.jsx
+++ src/customizations/components/theme/Navigation/NavItems.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavItem from '@plone/volto/components/theme/Navigation/NavItem';
-import { isInternalURL } from '@plone/volto/helpers';
+import config from '@plone/volto/registry';
 import { Dropdown } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 import './dropdownmenu.css';
 
 const NavItems = ({ items, lang, interior=false }) => {
+  const { settings } = config;
   return (
     <>
       {items.map((item) =>
@@ -12,21 +14,21 @@ const NavItems = ({ items, lang, interior=false }) => {
           <Dropdown simple
                     text={item.title}
                     className="item"
+                    to={item.url === '' ? '/' : item.url}
+                    exact={
+                      settings.isMultilingual
+                          ? item.url === `/${lang}` : item.url === ''
+                    }
+                    as={NavLink}
                     key={item.url} >
             <Dropdown.Menu>
-               <NavItem
-                 item={item}
-                 lang={lang}
-                 key={item.url}
-                 className="header" />
-              <Dropdown.Divider />
               <NavItems items={item.items} lang={lang} interior={true} />
             </Dropdown.Menu>
           </Dropdown>
         ) : (
           <NavItem item={item} lang={lang} key={item.url} />
         ),
-      )};
+      )}
     </>
   );
 };
